93ba858c21b65301d1fba13ab111d60b84ac1844
JS <python2_7@naver.com>
2019-08-29 17:18:40 +0900

    190829 settings ui

---
+++ lib/Screense/Login.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    //Context : 현재구동되는 앱의 상황
+    //final : 생성시점의 값을 변경 할 수 없음.
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        Container(
+          color: Colors.white,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            _setLogoImage(),
+            Stack(
+              children: <Widget>[
+                _setCard(size),
+                Positioned(
+                  left: size.width * 0.15,
+                  right: size.width * 0.15,
+                  bottom: 0,
+                  child: _setLoginBtn(),
+                ),
+              ],
+            ),
+            Container(
+              height: size.height * 0.1,
+            ),
+            Text('Dont have an acoount? create one'),
+            Container(
+              height: size.height * 0.05,
+            )
+          ],
+        )
+      ],
+    ));
+  }
+
+  Widget _setCard(Size size) {
+    return Padding(
+      padding: EdgeInsets.all(size.width * 0.05),
+      child: Card(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+        //formkey를 활용하여 TextForm의 상태를 가져올 수 있음
+        child: Padding(
+          padding: const EdgeInsets.only(
+              left: 12.0, right: 12.0, top: 12.0, bottom: 32.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.account_circle), labelText: 'email'),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return '아이디를 입력해주세요';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.vpn_key), labelText: 'password'),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return '암호를 입력해주세요';
+                    }
+                    return null;
+                  },
+                ),
+                Container(
+                  height: 16,
+                ),
+                Text('Forgot Paswword?')
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _setLoginBtn() {
+    return SizedBox(
+      height: 50,
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Text('LOGIN'),
+        color: Colors.blue,
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget _setLogoImage() {
+    //https://media.giphy.com/media/lqut5VxPEhP9zCJdUT/giphy.gif
+    //Expanded Row나 Column의 기준을 꽉 채움
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.only(top: 40, left: 24, right: 24),
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://media.giphy.com/media/lqut5VxPEhP9zCJdUT/giphy.gif'),
+          ),
+        ),
+      ),
+    );
+  }
+}
